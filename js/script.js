2f69c414f2dbf9ee0f4398be166fec83b35b5b45
hzlllns <hello@hzl.ca>
2015-12-08 16:33:17 -0500

    The blog posts show up

--- js/script.js
+++ js/script.js
@@ -1,10 +1,41 @@
 function listPosts(data) {
-  console.log(data);
+	console.log(data);
+
+	var output = '<form class="ui-filterable"><input id="searchposts" data-type="search"></form>';
+
+	output += '<ul data-role="listview" data-filter="true" data-input="#searchposts">';
+	$.each(data.posts, function(key, val) {
+
+		var tempDiv = document.createElement("tempDiv");
+		tempDiv.innerHTML = val.excerpt;
+		$("a", tempDiv).remove();
+		var excerpt = tempDiv.innerHTML;
+
+		output += '<li>';
+		output += '<a href="#blogpost" onclick = "showPost(' + val.id + ')">';
+		output += (val.thumbnail) ?
+		'<img src="' + val.thumbnail + '" alt="' + val.title + '">':
+		'<img src="images/viewsourcelogo.png" alt="View Source Logo">';
+		output += '<h3>' + val.title + "</h3>";
+		output += '<p>' + excerpt + "</p>";
+		output += '</a>';
+		output += '</li>';
+  }); //go through each post
+	output += "</ul>";
+	$('#postlist').html(output);
+} //listPosts
+
+function showPost(id) {
+	$.getJSON('http://hzl.ca/webd301_a3/category/weekly-log/?json=get_post&post_id=' + id + '&callback=?', function(data) {
+		var output = '<h2>' + data.post.title + '</h2>';
+		output += data.post.content;
+		$('#mypost').html(output);
+	});
 }
 
 $(function() {
-    $( "[data-role='navbar']" ).navbar();
-    $( "[data-role='header']" ).toolbar();
+	$( "[data-role='navbar']" ).navbar();
+	$( "[data-role='header']" ).toolbar();
 });
 
 $(document).on( "swipeleft swiperight", "#home", function( e ) {
@@ -21,19 +52,20 @@ $(document).on( "swipeleft swiperight", "#home", function( e ) {
 			});
 
 var mapCenter = new google.maps.LatLng(43.6500304,-79.3994166);
-	function initialize() {
-		var mapProp = {
-			center: mapCenter,
-			zoom: 18,
-			mapTypeId: google.maps.MapTypeId.ROADMAP
-		};
+function initialize() {
+	var mapProp = {
+		center: mapCenter,
+		zoom: 18,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
 
-		var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+	var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
 
-		var marker = new google.maps.Marker({
-			position: mapCenter
-		});
+	var marker = new google.maps.Marker({
+		position: mapCenter
+	});
 
-		marker.setMap(map);
-	}
-	google.maps.event.addDomListener(window, 'load', initialize);			
+	marker.setMap(map);
+}
+google.maps.event.addDomListener(window, 'load', initialize);			
+
